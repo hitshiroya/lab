5cd52bb37eb72499bfdde43f1e439205ef8eca2a
HitShiroya <hitp363530@gmail.com>
2026-02-11 22:49:49 +0530

    intial setup and implemented non stream response

---
+++ c-c/client/llm_client.py
@@ -0,0 +1,79 @@
+from groq import AsyncGroq
+from typing import Any , AsyncGenerator
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+
+
+from client.response import StreamEvent
+from client.response import TextDelta
+from client.response import TokenUsage
+from client.response import EventType
+
+
+api_key = os.getenv("LLM_API_KEY")
+model_name = os.getenv("MODEL_NAME")
+
+class LLMClient:
+    def __init__(self) -> None:
+       self._client : AsyncGroq | None = None
+
+    
+
+    def get_client(self) -> AsyncGroq:
+        if self._client is None:
+            self._client = AsyncGroq(
+                api_key=api_key,
+            )
+        return self._client
+    
+    
+
+    async def close(self) -> None:
+        if self._client:
+            await self._client.close()
+            self._client = None 
+    
+    async def chat_completion(self,messages: list[dict[str,Any]],stream:bool = True) -> AsyncGenerator[StreamEvent,None]:
+        client = self.get_client()
+
+        kwargs = {
+             "model": model_name,
+             "messages" : messages,
+             "stream" : stream
+        }
+
+        if stream:
+            self._stream_response()
+        else:
+            event = await self._non_stream_response(client,kwargs)
+            yield event
+
+    
+    async def _stream_response(self):
+            pass
+        
+    async def _non_stream_response(self, client : AsyncGroq , kwargs : dict[str,Any]) -> StreamEvent:
+        response = await client.chat.completions.create(**kwargs)
+        choice = response.choices[0]
+        message = choice.message
+        
+        text_delta = None
+        if message.content:
+            text_delta = TextDelta(content = message.content)
+
+        usage = None
+        if response.usage:
+            usage = TokenUsage(
+                prompt_tokens = response.usage.prompt_tokens,
+                completion_tokens = response.usage.completion_tokens,
+                total_tokens = response.usage.total_tokens,
+            )
+        
+        return StreamEvent(
+            type = EventType.MESSAGE_COMPLETE,
+            text_delta  = text_delta,
+            finish_reason = choice.finish_reason,
+            usage = usage
+        )
---
+++ c-c/main.py
@@ -0,0 +1,19 @@
+from client.llm_client import LLMClient
+import asyncio
+
+async def main():
+    client = LLMClient()
+    messages = [
+        {
+            "role" : "user",
+            "content" : "what is namespace in openshift?"
+        }
+    ]
+    async for event in client.chat_completion(messages,False):
+        print(event)
+
+
+
+asyncio.run(main())
+
+
