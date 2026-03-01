b2f006dbf01bb997fd084569b1b115a5750023e2
HitShiroya <hitp363530@gmail.com>
2026-03-01 13:57:35 +0530

    implemented crud with pydantic validation

--- fast-api/book.py
+++ fast-api/book.py
@@ -1,88 +1,97 @@
-from fastapi import Body, FastAPI 
-from pydantic import BaseModel , Field
+from fastapi import Body, FastAPI
+from pydantic import BaseModel, Field
 from typing import Optional
 
 app = FastAPI()
 
 
 class Book:
-    id : str
+    id: str
     name: str
     author: str
     rating: str
+    published_year: int
 
-
-    def __init__(self,id,name,author,rating):
+    def __init__(self, id, name, author, rating, published_year):
         self.id = id
         self.name = name
         self.author = author
         self.rating = rating
+        self.published_year = published_year
+
 
 class Book_Request(BaseModel):
-    id: Optional[int] = Field(description="id is not required during creation" , default=None)
-    name: str = Field(min_length= 3)
-    author: str = Field(min_length= 3)
-    rating: int = Field(gt=0,lt=6)
+    id: Optional[int] = Field(
+        description="id is not required during creation", default=None
+    )
+    name: str = Field(min_length=3)
+    author: str = Field(min_length=3)
+    rating: int = Field(gt=0, lt=6)
+    published_year: int
 
 
 books = [
-    Book(1,"Untold Story", "John Doe" , 5),
-    Book(2,"Untold Story1", "John Drake" , 5),
-    Book(3,"Untold Story2", "Jim Perry" , 2),
-    Book(4,"Untold Story3", "Merc Rons" , 4),
-    Book(5,"Untold Story4", "Jack Par" , 3),
+    Book(1, "Untold Story", "John Doe", 5, 2025),
+    Book(2, "Untold Story1", "John Drake", 5, 2024),
+    Book(3, "Untold Story2", "Jim Perry", 2, 2021),
+    Book(4, "Untold Story3", "Merc Rons", 4, 2021),
+    Book(5, "Untold Story4", "Jack Par", 3, 2019),
 ]
+
 
 def generate_book_id(book):
     book.id = 1 if len(books) == 0 else books[-1].id + 1
     return book
-    
-@app.delete("/api/delete-book/{delete_title}")
-async def delete_book(delete_title: str):
-    print(delete_title)
+
+
+@app.delete("/api/books/{book_id}}")
+async def delete_book(delete_id: int):
     for i in range(len(books)):
-        if books[i].get("name") == delete_title:
+        if books[i].id == delete_id:
             books.pop(i)
             break
 
 
 @app.post("/api/create-book")
-async def create_books(book_request:Book_Request):
+async def create_books(book_request: Book_Request):
     new_book_store = Book(**book_request.model_dump())
     books.append(generate_book_id(new_book_store))
 
 
 @app.put("/api/update_book")
-async def update_book(updated_book=Body()):
+async def update_book(updated_book: Book_Request):
     for i in books:
-        if i.get("name") == updated_book.get("name"):
-            i["author"] = "Jims perry"
+        if i.name == updated_book.name:
+            updated_book.author = "Jonty"
+    return updated_book
 
 
-@app.get("/api/books/{books_by_author}")
-async def get_books_by_author(books_by_author: str):
-    book_author = []
+@app.get("/api/books/")
+async def get_books_rating(book_rating: int):
+    new_book_by_rating = []
+    for i in books:
+        if i.rating == book_rating:
+            new_book_by_rating.append(i)
+
+    return new_book_by_rating
+
+
+@app.get("/api/books/{book_id}")
+async def get_book_by_id(book_id: int):
     for i in range(len(books)):
-        if books[i].get("author") == books_by_author:
-            book_author.append(books[i])
-    return book_author
+        if books[i].id == book_id:
+            return books[i]
 
 
-@app.get("/api/books/{book_identity}")
-async def get_specific_book(book_identity):
+@app.get("/api/books/{publish_year}")
+async def get_book_publish_year(publish_year: int):
+    new_book_publish_year = []
+
     for i in books:
-        if i["author"] == book_identity:
-            return {"data": i}
+        if i.published_year == publish_year:
+            new_book_publish_year.append(i)
 
-
-@app.get("/api/books/{author}")
-async def get_books_by_author_and_status(author: str, pubslished: str):
-    ans = []
-    for i in books:
-        if i.get(author) == author and i.get(pubslished) == pubslished:
-            ans.append(i)
-
-    return ans
+    return new_book_publish_year
 
 
 @app.get("/api/books")
