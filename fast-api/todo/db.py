5694ec04bdc41bdec938fc01b45d0e254e7d595f
HitShiroya <hitp363530@gmail.com>
2026-03-04 14:56:49 +0530

    implemeted user registration flow

--- fast-api/todo/db.py
+++ fast-api/todo/db.py
@@ -13,5 +13,3 @@ SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind = engine)
 
 Base = declarative_base()
 
-
-
--- fast-api/todo/models.py
+++ fast-api/todo/models.py
@@ -1,5 +1,19 @@
 from db import Base
-from sqlalchemy import Column , Integer, Boolean , String
+from sqlalchemy import Column , Integer, Boolean , String, ForeignKey
+
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer,primary_key=True, index=True)
+    email = Column(String,unique=True)
+    username = Column(String,unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
+
 
 
 class Todo(Base):
@@ -10,6 +24,7 @@ class Todo(Base):
     description = Column(String)
     priority =  Column(Integer)
     complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
 
 
 
